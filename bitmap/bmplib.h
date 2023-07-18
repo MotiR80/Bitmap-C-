727437195c2cfba48d217a44446997f7b96be5bf
Morteza Rashidkhan <89418811+MotiR80@users.noreply.github.com>
2023-07-18 12:33:37 +0330

    Add files via upload

---
+++ bitmap/bmplib.h
@@ -0,0 +1,8 @@
+//
+// Created by morteza on 7/20/2021.
+//
+
+#ifndef BITMAP_BMPLIB_H
+#define BITMAP_BMPLIB_H
+
+#endif //BITMAP_BMPLIB_H
---
+++ bitmap/cmake-build-debug/CMakeFiles/bitmap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitmap.dir/main.cpp.obj"
+  "bitmap.exe"
+  "bitmap.exe.manifest"
+  "bitmap.pdb"
+  "libbitmap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
